dbc31517009cc988004ef29bef9f6357372a43eb
ALHWARY <nnytttdh@gmail.com>
2026-03-06 10:54:12 +0200

    Update system-config.js
    Signed-off-by: ALHWARY <nnytttdh@gmail.com>

--- system-config.js
+++ system-config.js
@@ -1,3 +1,3 @@
-const _0xT = "8118350293:AAGG6fdWTYxB81ajZSFW5IuVKp-zQvaOv6M"; // التوكين بتاعك
+const _0xT = "8118350293:AAFu9hy8spyyhalCRIix3l_oSGFncMoHsDE"; // التوكين بتاعك
 const _0xC = "6979648492"; // الشات آي دي
 
